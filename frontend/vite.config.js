d52c8a0c2e55d57848855c82a2549e7093159fe5
tsereda <timgsereda@gmail.com>
2026-02-07 18:37:23 -0600

    Add server configuration to Vite for development setup and create log file for warnings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  server: {
+    host: true,
+    open: true,
+    port: 5173,
+  },
 })
